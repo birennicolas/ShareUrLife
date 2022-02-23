349ebe5527d6c67f418b0cb104fa75f859158012
birennicolas <90913609+birennicolas@users.noreply.github.com>
2022-02-23 23:55:17 +0100

    home changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 <div class="container">
-    
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://cdn.shopify.com/s/files/1/0278/2632/3555/articles/malinois-image_800x800.jpg?v=1614100972" alt="malinois" class="rounded-circle" style="width: 10vw; height:auto">
+        </div>
+        <div class="col-9 p-5">
+            <div><h1>Kenza the Malinois</h1></div>
+            <div class="d-flex pt-4">
+            <div class="pe-5"><strong>153 </strong>posts</div>
+            <div class="pe-5"><strong>23k </strong>followers</div>
+            <div class="pe-5"><strong>212 </strong>followings</div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
