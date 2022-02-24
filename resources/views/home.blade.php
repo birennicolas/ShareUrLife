a5dde4cc6d7f74ecd2dcba9378d02224db3b2bcf
birennicolas <90913609+birennicolas@users.noreply.github.com>
2022-02-24 14:41:54 +0100

    home update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,24 @@
             <div class="pe-5"><strong>23k </strong>followers</div>
             <div class="pe-5"><strong>212 </strong>followings</div>
             </div>
+            <div class="pt-4 font-weight-bold">kenzathemalinois</div>
+            <div class="pt-4">I am one of the craziest dogs you can ever imagine</div>
+            <div ><a href="https://www.woopets.fr/chien/race/berger-belge-malinois">Malinois</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://cdn.shopify.com/s/files/1/0278/2632/3555/articles/malinois-image_800x800.jpg?v=1614100972" alt="malinois1" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://images.onlinepets.com/uploads/2021/02/Mechelse-Herder.jpg?auto=format" alt="malinois2" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://www.woopets.fr/assets/races/000/031/square300/berger-belge-malinois.jpg" alt="malinois3" class="w-100">
         </div>
     </div>
 </div>
+
+
 @endsection
