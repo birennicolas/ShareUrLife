31ec23006b28a84cc606d5055d718498d9c62101
birennicolas <90913609+birennicolas@users.noreply.github.com>
2022-02-25 18:07:58 +0100

    Profile picture

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,7 +9,13 @@ class Profile extends Model
 {
 
     protected $guarded = [];
-    
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : '/profile/5UfXLkkJhe5OB4xnjCaplG0kbWPGNLjYjTUpEfE7.jpg';
+        return  '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,9 +8,35 @@
         </div>
 
         <div class="col-4">
-            <h3>{{ $post->user->username }}</h3>
+            <div class="#">
+                <div class="d-flex align-items-center">
+                    <div class="pe-3">
+                        <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-width: 100px;">
+                    </div>
+                    <div class="div">
+                        <div>
+                            <strong>
+                                <a href="/profile/{{ $post->user->id }}">
+                                    <span class="text-dark">{{ $post->user->username }}</span>
+                                </a>
+                            </strong>
+                                <a href="#" class="ps-3">Follow</a>
+                        </div>
+                    </div>
+                </div>
 
-            <p>{{ $post->caption }}</p>
+                <hr>
+
+                <p>
+                    <span>
+                        <strong>
+                            <a href="/profile/{{ $post->user->id }}">
+                                <span class="text-dark">{{ $post->user->username }}</span>
+                            </a>
+                        </strong>
+                    </span>{{ $post->caption }}
+                </p>
+            </div>
         </div>
     </div>
 </div>
