30a56c318ac2851a40ff958dbd1234f42d211f4c
birennicolas <90913609+birennicolas@users.noreply.github.com>
2022-03-11 16:43:17 +0100

    follow btn loggedin

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="d-flex align-items-center">
                     <h1> {{ $user -> username }}</h1>
 
-                    <follow-button></follow-button>
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
                 </div>
 
                 @can('update', $user->profile)
